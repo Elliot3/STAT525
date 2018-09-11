2d709e5987e72c14bba9ff305cabfbebed5c8f6a
Elliot Smith <esmith2642@gmail.com>
2018-09-10 22:15:57 -0500

    more work on hw03 prob 2

--- HW03/HW03.R
+++ HW03/HW03.R
@@ -1,13 +1,37 @@
 ## Load in the necessary packages
 
 library(gamair)
+library(coda)
 
 ## Load in the data
 
 data("hubble")
 
-## Define the posterior distribution
+x <- hubble$x
+y <- hubble$y
 
-errors <- rnorm()
+## Setup the parameters
 
-post_dist <- lm(y ~ x, data = hubble)
+alpha <- 0.01
+beta <- 0.01
+n <- 24
+theta_hat <- sum(x * y) / sum(x^2)
+theta_hat <- 1/theta_hat*3.09e19/(60^2*24*365)
+s_squared <- (1 / (n - 1)) * sum(y - (x * theta_hat))^2
+
+## Make 10,000 draws from [tau_y|y], tau^s
+
+draws_1 <- rgamma(n = 10000, shape = alpha + (n - 1), rate = beta + (n - 1) * (s_squared / 2))
+
+## Make 10,000 draws from [theta|y, tau_y = t^s]
+
+var_y <- var(y)
+
+draws_2 <- rnorm(n = 10000,
+                 mean = ((1/var_y) * sum(x^2))^(-1) * ((1/var_y) * sum(x * y))^(-1),
+                 sd = ((1/var_y) * sum(x^2))^(-1)
+                 )
+
+## Construct the HPD interval
+
+HPDinterval(as.mcmc(draws_2), prob = 0.95)
