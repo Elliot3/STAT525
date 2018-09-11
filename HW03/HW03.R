870de80e15ee40f955728ea911316eee9e0ebba2
Elliot Smith <esmith2642@gmail.com>
2018-09-10 23:24:36 -0500

    a few more hw03 adds:

--- HW03/HW03.R
+++ HW03/HW03.R
@@ -1,3 +1,7 @@
+## Set options
+
+options(scipen = 999)
+
 ## Load in the necessary packages
 
 library(gamair)
@@ -16,20 +20,21 @@ alpha <- 0.01
 beta <- 0.01
 n <- 24
 theta_hat <- sum(x * y) / sum(x^2)
-theta_hat <- 1/theta_hat*3.09e19/(60^2*24*365)
 s_squared <- (1 / (n - 1)) * sum(y - (x * theta_hat))^2
 
 ## Make 10,000 draws from [tau_y|y], tau^s
 
-draws_1 <- rgamma(n = 10000, shape = alpha + (n - 1), rate = beta + (n - 1) * (s_squared / 2))
+draws_1 <- rgamma(n = 10000, shape = alpha + ((n - 1) / 2), rate = beta + (n - 1) * (s_squared / 2))
 
 ## Make 10,000 draws from [theta|y, tau_y = t^s]
 
-var_y <- var(y)
+post_hub <- 1/draws_1*3.09e19/(60^2*24*365)
+
+var_y <- post_hub
 
 draws_2 <- rnorm(n = 10000,
                  mean = ((1/var_y) * sum(x^2))^(-1) * ((1/var_y) * sum(x * y))^(-1),
-                 sd = ((1/var_y) * sum(x^2))^(-1)
+                 sd = sqrt(((1/var_y) * sum(x^2))^(-1))
                  )
 
 ## Construct the HPD interval
