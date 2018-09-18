b0ddea539b11b372a36a19ddc46621ccc00a3978
Elliot Smith <esmith2642@gmail.com>
2018-09-18 00:14:31 -0500

    some work into hw04 p01

---
+++ HW04/HW04.R
@@ -0,0 +1,66 @@
+## Load necessaey packages
+
+library(coda)
+
+
+
+########## Problem 1 ##########
+
+
+
+##### Part i #####
+
+
+
+num_draws <- 10^4
+
+mean_c <- 1.013
+mean_t <- 1.173
+
+## Draw from the scaled inverse chi-square
+
+n_1 <- 32
+df <- n_1 - 1
+
+samp_i_1 <- rinvchisq(num_draws, df, scale=1/df)
+
+## Draw from the normal distribution
+
+samp_i_2 <- rnorm(num_draws, mean = mean_c, sqrt(samp_i_1))
+
+
+
+##### Part ii #####
+
+## Draw from the scaled inverse chi-square
+
+n_2 <- 36
+df <- n_2 - 1
+
+samp_ii_1 <- rinvchisq(num_draws, df, scale=1/df)
+
+## Draw from the normal distribution
+
+samp_ii_2 <- rnorm(num_draws, mean = mean_t, sqrt(samp_ii_1))
+
+
+
+##### Part iii #####
+
+## Generate the histogram of mean differences
+
+hist(samp_ii_2 - samp_i_2,
+     xlab = "Mean Difference",
+     ylab = "",
+     main ="Histogram: Mean Difference")
+
+
+
+##### Part iv #####
+
+## Construct the HPD interval of mean differences
+
+HPDinterval(as.mcmc(samp_ii_2 - samp_i_2), prob = 0.95)
+
+
+
