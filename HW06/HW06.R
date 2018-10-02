86b84162f0824bb096e0844ba9a1ed28e4d10e8b
Elliot Smith <esmith2642@gmail.com>
2018-10-01 21:54:19 -0500

    finished draft prob2

--- HW06/HW06.R
+++ HW06/HW06.R
@@ -30,6 +30,28 @@
 
 
 
+nu <- 4
+S <- 10000
+
+post_gamma <- numeric()
+
+for (s in 1:S) {
+    
+    post_gamma[s] <- rgamma(n = 1, shape = (nu / 2), rate = (nu / 2))
+    
+}
+
+post_norm <- dnorm(x = post_gamma, mean = 0, sd = sqrt(1 / post_gamma))
+
+t_dens <- dt(x = post_gamma, df = nu)
+
+par(mfrow = c(1, 2))
+
+hist(post_norm)
+hist(t_dens)
+
+
+
 ##### Part iii #####
 
 
