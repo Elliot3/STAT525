ef474f9ca97f78f3b26f88da534cee2c6200b98d
Elliot Smith <esmith2642@gmail.com>
2018-10-04 17:24:56 -0500

    starting prob 1 part i

---
+++ HW07/HW07.R
@@ -0,0 +1,51 @@
+########## Workspace Prep ##########
+
+
+
+########## Problem 1 ##########
+
+
+
+## Load in the lh data
+
+data(lh)
+y <- as.numeric(lh)
+Y <- length(y)
+
+
+
+##### Part i #####
+
+
+
+##### Part ii #####
+
+
+
+##### Part iii #####
+
+
+
+##### Part iv #####
+
+
+
+##### Part v #####
+
+
+
+########## Problem 2 ##########
+
+
+
+##### Part i #####
+
+
+
+##### Part ii #####
+
+
+
+##### Part iii #####
+
+
